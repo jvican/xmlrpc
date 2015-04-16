c6e0f5516e3384198e0a91ad6ef6d6d693ac3c04
jvican <jorgevc@fastmail.es>
2015-04-16 15:44:44 +0200

    Timezone integration comment

--- src/main/scala/xmlrpc/DatetimeSpec.scala
+++ src/main/scala/xmlrpc/DatetimeSpec.scala
@@ -9,7 +9,10 @@ trait DatetimeSpec {
    * information. Specification in [[http://en.wikipedia.org/wiki/XML-RPC]].
    */
   val ISO8601Format = new SimpleDateFormat("yyyyMMdd'T'HH:mm:ss")
+
+  // If you want to change the timezone of the dates, just override this
   val serverTimezone = TimeZone.getDefault
+
   ISO8601Format.setTimeZone(serverTimezone)
   ISO8601Format.setLenient(false)
 
